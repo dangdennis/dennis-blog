66f61b8c41ce1ab94cc8c07b1759ef58184e1ade
Dennis Dang <dangggdennis@gmail.com>
2018-10-27 20:01:27 -0700

    Change to mah name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
+    author: 'Dennis Dang',
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
   },
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -18,18 +18,19 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Kyle Mathews`}
+          alt={`Dennis Dang`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
             width: rhythm(2),
             height: rhythm(2),
+            borderRadius: "50%"
           }}
         />
         <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
+          Written by <strong>Dennis Dang</strong> who lives and works in San
           Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
+          <a href="https://twitter.com/dangitdennis">
             You should follow him on Twitter
           </a>
         </p>
