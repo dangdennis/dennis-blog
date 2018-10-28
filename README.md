66eec5f3732882befa36ebaefbaba3b524c16046
Dennis Dang <dangggdennis@gmail.com>
2018-10-27 23:56:03 -0700

    Update readme and font sizing

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# gatsby-starter-blog
-Gatsby starter for creating a blog
+# Only my personal website and blog
 
-Install this starter (assuming Gatsby is installed) by running from your CLI:
-
-`gatsby new gatsby-blog https://github.com/gatsbyjs/gatsby-starter-blog`
-
-## Running in development
-`gatsby develop`
+I'm glad you're visiting my GitHub!
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,32 @@
 import Typography from 'typography'
 import moragaTheme from 'typography-theme-moraga'
 
+moragaTheme.headerFontFamily = [
+  '-apple-system',
+  'system-ui',
+  'BlinkMacSystemFont',
+  'Segoe UI',
+  'Roboto',
+  'Helvetica Neue',
+  'Arial',
+  'sans-serif',
+]
+
+moragaTheme.bodyFontFamily = [
+  '-apple-system',
+  'system-ui',
+  'BlinkMacSystemFont',
+  'Segoe UI',
+  'Roboto',
+  'Helvetica Neue',
+  'Arial',
+  'sans-serif',
+]
+
+moragaTheme.headerWeight = '400'
+
+delete moragaTheme.googleFonts
+
 const typography = new Typography(moragaTheme)
 
 // Hot reload typography in development.
