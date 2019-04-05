7bef966ef3b859a80f571cd7ab93c2b1382c21c0
Dennis Dang <dangggdennis@gmail.com>
2019-04-05 00:11:39 -0700

    Update interests, tech stack, typo fixes

--- src/components/About.js
+++ src/components/About.js
@@ -47,7 +47,7 @@ export default function About() {
               <div>
                 <h3 style={H3Style}>Trending Interests</h3>
                 <p style={{ marginBottom: rhythm(0) }}>
-                  Rust, Go, blockchain, serverless, and finite state machines. Lol, who am I?
+                  Rust, Flutter, Go, blockchain, serverless.
                 </p>
                 <p>
                   I used to consider myself a hip-hop dancer. But now I'm just a
